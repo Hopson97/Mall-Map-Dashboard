9099ce4ddd33fcf74b249614caa041044ada0ebc
Hopson97 <mhopson@hotmail.co.uk>
2019-04-10 07:26:46 +0100

    Improve look of navbar

--- public/js/navbar-gen.js
+++ public/js/navbar-gen.js
@@ -3,11 +3,11 @@
 window.addEventListener("load", e => {
     const nav = document.getElementById("navbar");
 
-    const addButton = (name, url) => {
+    const addButton = (name, url, image) => {
         const sect = document.getElementById("nav-section");
         const clone = document.importNode(sect.content, true);
         clone.querySelector('a').href = url;
-        clone.querySelector('div').textContent = name;
+        clone.querySelectorAll('div')[1].textContent = name;
 
         nav.appendChild(clone);
     };
